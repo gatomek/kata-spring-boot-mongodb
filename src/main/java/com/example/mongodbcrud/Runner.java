0b217aab4556edbfdceb6e48c9cea0a434ad8528
Gatomek <tomasz.galinski@gmail.com>
2023-03-13 23:32:51 +0100

    Storing image (BSON) added

--- src/main/java/com/example/mongodbcrud/Runner.java
+++ src/main/java/com/example/mongodbcrud/Runner.java
@@ -1,10 +1,18 @@
 package com.example.mongodbcrud;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
+import org.apache.commons.io.FileUtils;
+import org.bson.BsonBinarySubType;
+import org.bson.types.Binary;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,7 +21,8 @@ import java.util.UUID;
 public class Runner implements CommandLineRunner {
 
     FlashcardRepository repository;
-    public Runner( FlashcardRepository repository) {
+
+    public Runner(FlashcardRepository repository) {
         this.repository = repository;
     }
 
@@ -21,28 +30,55 @@ public class Runner implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         List<Flashcard> entries = repository.findAll();
-        entries.stream().forEach( e-> System.out.println( e.toString() ));
+        entries.stream().forEach(e -> System.out.println(e.toString()));
 
         long count = repository.count();
-        System.out.println( count);
+        System.out.println(count);
 
-        QFlashcard qflashcard = new QFlashcard( "abc");
-        BooleanExpression expression = qflashcard.uuid.eq( "001599b0-60e0-43ca-81b7-782b173d900b");
-        Iterable<Flashcard> oneItemlist = repository.findAll( expression);
+        QFlashcard qflashcard = new QFlashcard("abc");
+        BooleanExpression expression = qflashcard.uuid.eq("001599b0-60e0-43ca-81b7-782b173d900b");
+        Iterable<Flashcard> oneItemlist = repository.findAll(expression);
 
         System.out.println();
-        oneItemlist.forEach( e -> System.out.println( e.toString()));
+        oneItemlist.forEach(e -> System.out.println(e.toString()));
 
 
         Flashcard flashcard = new Flashcard();
-        flashcard.setUserName( "Wodnik Szuwarek");
-        flashcard.setFolder( "Puszcza");
-        flashcard.setPolish(Arrays.asList( "Gdzie szumią wierzby" ));
-        flashcard.setForeign( Arrays.asList("W Puszczy"));
+        flashcard.setUserName("Wodnik Szuwarek");
+        flashcard.setFolder("Puszcza");
+        flashcard.setPolish(Arrays.asList("Gdzie szumią wierzby"));
+        flashcard.setForeign(Arrays.asList("W Puszczy"));
         String uuid = UUID.randomUUID().toString();
-        flashcard.setUuid( uuid);
-        flashcard.setHashtags( Arrays.asList( "Testy"));
+        flashcard.setUuid(uuid);
+        flashcard.setHashtags(Arrays.asList("Testy"));
 
-        repository.save( flashcard);
+
+        File file = ResourceUtils.getFile("classpath:img/67f1da42-2eef-458a-9572-e4d1bfcad4d3.jpg");
+        InputStream in = new FileInputStream(file);
+
+        byte[] data = in.readAllBytes();
+        Binary image = new Binary(BsonBinarySubType.BINARY, data);
+        flashcard.setImage(image);
+
+        repository.save(flashcard);
+        //repository.insert(flashcard);
+
+        {
+            QFlashcard qflashcard2 = new QFlashcard("abc");
+            BooleanExpression expression2 = qflashcard2.uuid.eq(uuid);
+            Iterable<Flashcard> oneItemlist2 = repository.findAll(expression2);
+
+            Iterator<Flashcard> iterator = oneItemlist2.iterator();
+
+            while (iterator.hasNext()) {
+                Flashcard fc = iterator.next();
+                Binary binary = fc.getImage();
+                byte[] data2 = binary.getData();
+
+                File outFile = new File("c:/mongodb/67f1da42-2eef-458a-9572-e4d1bfcad4d3.jpg");
+                FileUtils.writeByteArrayToFile(outFile, data2);
+                break;
+            }
+        }
     }
 }
